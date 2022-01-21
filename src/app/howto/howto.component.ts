ee3d69fed3a59e93c2dbb66f3c1ec6f890b8b403
jbratu <jared@thecongruity.com>
2022-01-21 10:44:13 -0500

    Add how-to selector

--- src/app/howto/howto.component.ts
+++ src/app/howto/howto.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-howto',
   templateUrl: './howto.component.html',
-  styleUrls: ['./howto.component.css']
+  styleUrls: ['./howto.component.css'],
 })
 export class HowtoComponent implements OnInit {
+  selectedHowTo = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  selectHowto(event: any) {
+    // console.log(event.target.selectedIndex);
+    this.selectedHowTo = event.target.value;
   }
-
 }
