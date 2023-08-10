1f25b181594b25bb4a10b090e80475a3fefc1036
EC2AMAZ-RPVSAI7\wasim <wasim@srpcs.com>
2023-08-10 13:59:15 +0000

    moved files into separate folders

--- src/app/state/refresh/refresh.selectors.ts
+++
@@ -1,4 +0,0 @@
-import { Book } from "./../../howto/manage-state-with-ngrx/books.model";
-import { createFeatureSelector } from "@ngrx/store";
-
-export const selectRefresh = createFeatureSelector<string>("refresh");
